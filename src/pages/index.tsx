0f1313ef6f9612d33ee1e79f1d3259f0207488e3
nandan <nandankmrjha@gmail.com>
2023-05-24 23:24:21 +0530

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,8 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.posts.getAll.useQuery();
+  console.log(data);
 
   const user = useUser();
 
@@ -18,14 +19,11 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <SignIn path="/sign-in" routing="path" />
+        <div>{user.isSignedIn ? <SignOutButton /> : <SignInButton />}</div>
         <div>
-          {
-            user.isSignedIn ? (
-              <SignOutButton />
-            ) : (
-              <SignInButton />
-            )
-          }
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
